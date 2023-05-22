c48eec6510620cc9c5333a2f2a033dcccc469793
Gabriel Almeida <gabriel.almeida@cfo.org.br>
2023-05-22 14:30:17 -0300

    criada função equalizacao

--- src/IPI_02.py
+++ src/IPI_02.py
@@ -1,9 +1,42 @@
 import cv2 
-import numpy as np 
-img = cv2.imread('input\car.png', 0)
-equ = cv2.equalizeHist(img) 
-res = np.hstack((img, equ)) 
-cv2.imshow('resultado', res) 
+import numpy as np
+import matplotlib.pyplot as plt
+
+# img = cv2.imread('../input/car.png', 0)
+# equ = cv2.equalizeHist(img) 
+# res = np.hstack((img, equ)) 
+# cv2.imshow('',res) 
   
-cv2.waitKey(0) 
-cv2.destroyAllWindows() 
+# cv2.waitKey(0) 
+# cv2.destroyAllWindows()
+
+def equalize(image):
+    # # recebe o caminho da imagem
+    # image = cv2.imread(path,0)
+    
+    # Calcular o histograma
+    histogram, bins = np.histogram(image.flatten(), bins=256, range=[0, 256])
+    # Calcular a CDF
+    cdf = histogram.cumsum()
+    # Normalizar a CDF
+    cdf_normalized = cdf / cdf.max()
+    # Aplicar a equalização de histograma
+    equalized_image = np.interp(image.flatten(), bins[:-1], cdf_normalized * 255).reshape(image.shape).astype(np.uint8)
+    return equalized_image
+
+#__________main__________
+image = cv2.imread(r'../input/car.png',0)
+equalized_image = equalize(image)
+
+cv2.imshow('teste',equalized_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# # Exibir a imagem original e a imagem equalizada
+# plt.subplot(1, 2, 1)
+# plt.imshow(image, cmap='gray')
+# plt.title('Imagem original')
+# plt.subplot(1, 2, 2)
+# plt.imshow(equalized_image, cmap='gray')
+# plt.title('Imagem equalizada')
+# plt.show()
--- src/IPI_03.py
+++ src/IPI_03.py
@@ -1,11 +1,12 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Carrega a imagem
-image = cv2.imread('input/car.png')
+image = cv2.imread(r'../input/car.png',0)
 
 # Define o valor do parâmetro de gama
-gamma = 1.5
+gamma = 0.5
 
 # Cria a tabela de pesquisa (lookup table) para a correção de gama
 gamma_table = np.array([((i/255.0) ** (1/gamma)) * 255 for i in np.arange(0, 256)]).astype("uint8")
@@ -13,8 +14,41 @@ gamma_table = np.array([((i/255.0) ** (1/gamma)) * 255 for i in np.arange(0, 256
 # Aplica a correção de gama na imagem usando a tabela de pesquisa
 corrected_image = cv2.LUT(image, gamma_table)
 
-# Mostra a imagem original e a imagem corrigida
-cv2.imshow('Imagem Original', image)
-cv2.imshow('Imagem Corrigida', corrected_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# # Mostra a imagem original e a imagem corrigida
+# cv2.imshow('Imagem Original', image)
+# cv2.imshow('Imagem Corrigida', corrected_image)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+# Calcular o histograma
+histogram, bins = np.histogram(image.flatten(), bins=256, range=[0, 256])
+
+# Calcular a CDF
+cdf = histogram.cumsum()
+
+# Normalizar a CDF
+cdf_normalized = cdf / cdf.max()
+
+# # Plotar o histograma
+# plt.plot(histogram)
+# plt.xlabel('Intensidade')
+# plt.ylabel('Frequência')
+# plt.show()
+
+# # Plotar a CDF
+# plt.plot(cdf_normalized)
+# plt.xlabel('Intensidade')
+# plt.ylabel('CDF Normalizada')
+# plt.show()
+
+# Aplicar a equalização de histograma
+equalized_image = np.interp(image.flatten(), bins[:-1], cdf_normalized * 255).reshape(image.shape).astype(np.uint8)
+
+# Exibir a imagem original e a imagem equalizada
+plt.subplot(1, 2, 1)
+plt.imshow(image, cmap='gray')
+plt.title('Imagem original')
+plt.subplot(1, 2, 2)
+plt.imshow(equalized_image, cmap='gray')
+plt.title('Imagem equalizada')
+plt.show()
