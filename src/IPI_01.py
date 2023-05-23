8a2d6156dd014e51a9f1e3082a173253e270d42f
Gabriel Almeida <gabriel.almeida@cfo.org.br>
2023-05-23 16:44:04 -0300

    v0.4

--- src/IPI_01.py
+++ src/IPI_01.py
@@ -42,6 +42,27 @@ def pair_fill(matriz, white_coll):
  
     return matriz
 
+def m_odd_fill(matriz, white_lines):
+    for row in white_lines:
+        linha = np.copy(matriz[row-1])
+        linha_2 = np.copy(matriz[row-1])
+        if(linha_2 is not None):
+            linha = (linha + linha_2)/2
+        matriz[row] = linha
+                    
+    return matriz
+
+def m_pair_fill(matriz, white_coll):
+    for row in range(len(matriz)):
+        for item in white_coll:
+            cell = matriz[row][item-1]
+            if(item+1 < len(matriz)):
+               cell_2 = cell = matriz[row][item+1]
+               cell = (cell + cell_2)/2
+            matriz[row][item] = cell
+ 
+    return matriz
+
 def tam2(image):
     fill_image = fill(image)
 
@@ -53,15 +74,37 @@ def tam2(image):
 
     return fill_image
 
+def tamm(image):
+    fill_image = fill(image)
+
+    for x in range(fill_image.shape[2]):
+        white_lines = verifica_row(fill_image[:,:,x])
+        fill_image[:,:,x] = m_odd_fill(fill_image[:,:,x], white_lines)
+        white_coll = verifica_coll(fill_image[:,:,x])
+        fill_image[:,:,x] = m_pair_fill(fill_image[:,:,x], white_coll)
+
+    return fill_image
+
+def print2Pl(old_image, new_image, arg_a ='Imagem original', arg_b ='Imagem equalizada'):
+    # Exibir a imagem original e a imagem equalizada ( equalização )
+    plt.subplot(1, 2, 1)
+    plt.imshow(old_image, cmap='gray')
+    plt.title(arg_a)
+    plt.subplot(1, 2, 2)
+    plt.imshow(new_image, cmap='gray')
+    plt.title(arg_a)
+    plt.show()
 
 #______________________MAIN______________________
 image = cv2.imread(r'../input/fruit1.jpg')
-#image = np.ones((4, 4, 3), dtype=np.uint8)
+# image = np.ones((3, 3, 3), dtype=np.uint8)
 
-#print(image,"____________AQUI____________\n")
+image2 = tamm(image)
 image = tam2(image)
 cv2.imwrite('../output/fruit1.jpg',image)
+cv2.imwrite('../output/fruit2.jpg',image2)
 
+print2Pl(image,image2)
 
 for size in range(image.shape[2]):
     print(image[:,:,size],'\n')
