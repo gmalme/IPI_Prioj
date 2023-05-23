dc32b91917910a60c37ec3080c40974c00f95921
Gabriel <gbmalme@gmail.com>
2023-05-22 21:38:51 -0300

    organização minima questão 2

--- src/IPI_02.py
+++ src/IPI_02.py
@@ -2,14 +2,6 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-# img = cv2.imread('../input/car.png', 0)
-# equ = cv2.equalizeHist(img) 
-# res = np.hstack((img, equ)) 
-# cv2.imshow('',res) 
-  
-# cv2.waitKey(0) 
-# cv2.destroyAllWindows()
-
 def equalize(image):
     # # recebe o caminho da imagem
     # image = cv2.imread(path,0)
@@ -22,7 +14,7 @@ def equalize(image):
     cdf_normalized = cdf / cdf.max()
     # Aplicar a equalização de histograma
     equalized_image = np.interp(image.flatten(), bins[:-1], cdf_normalized * 255).reshape(image.shape).astype(np.uint8)
-    return equalized_image
+    return equalized_image, histogram, cdf_normalized
 
 def gamma(image,gamma):
     gamma_table = np.array([((i/255.0) ** (1/gamma)) * 255 for i in np.arange(0, 256)]).astype("uint8")
@@ -61,19 +53,20 @@ def print2Pl(old_image, new_image, arg_a ='Imagem original', arg_b ='Imagem equa
 
 # __________main__________
 image = cv2.imread(r'../input/car.png',0)
-# equalized_image = equalize(image)
+equalized_image, histogram, cdf = equalize(image)
 corrected_image = gamma(image, 0.6)
 
 # printa imagem equalizada
 # printIm('imagem equalizada', equalized_image) 
 # print2Pl(image, equalized_image)
 
-# print gama
-print2Im(image, corrected_image)
+# # print histograma e cdf
+# printPl(cdf)
 
-# # Plotar o histograma or CDF
-# plt.plot(histogram)
-# plt.xlabel('Intensidade')
-# plt.ylabel('Frequência')
-# plt.show()
+# print gama
+# print2Im(image, corrected_image)
+
+
+
+
 
