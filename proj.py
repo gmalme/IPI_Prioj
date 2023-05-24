7ca4c01a98380213f57d856ea68271b8c5a2e012
Gabriel Almeida <gabriel.almeida@cfo.org.br>
2023-05-24 14:56:52 -0300

    v0.9 - versão funcional

--- proj.py
+++ proj.py
@@ -1,21 +1,45 @@
+from src.imageResize import imageResize
 from src.imageTreatment import imageTreatment
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-image_treatment = imageTreatment()
+# _____________ Questao 01 _____________
+image_resize = imageResize()
 images_01 = []
 images_01.append(cv2.imread(r'input/fruit1.jpg'))
 images_01.append(cv2.imread(r'input/fruit2.jpg'))
 
 # Questão 1.1:
-image_result = image_treatment.tam2(images_01[0])
+image_result = image_resize.tam2(images_01[0])
 cv2.imwrite('output/quest_1.1.jpg',image_result)
 
 # Questão 1.2:
-image_result = image_treatment.tamm(images_01[0])
+image_result = image_resize.tamm(images_01[0])
 cv2.imwrite('output/quest_1.2.jpg',image_result)
 
 # Questão 1.3:
-image_result = image_treatment.merge_image(images_01[0],images_01[1])
+image_result = image_resize.merge_image(images_01[0],images_01[1])
 cv2.imwrite('output/quest_1.3.jpg',image_result)
+
+# _____________ Questao 02 _____________
+image_treatment = imageTreatment()
+images_02 = []
+images_02.append(cv2.imread(r'input/car.png'))
+images_02.append(cv2.imread(r'input/crowd.png'))
+images_02.append(cv2.imread(r'input/university.png'))
+
+# Questão 2.1:
+corrected_image = image_treatment.gamma(images_02[0], 0.4)
+image_treatment.print2Pl(images_02[0], corrected_image)
+cv2.imwrite('output/quest_2.1.jpg',corrected_image)
+
+# Questão 2.2:
+# equalized_image, histogram, cdf = image_treatment.equalize(images_02[0])
+# image_treatment.printPl(histogram)
+# image_treatment.printPl(cdf)
+# image_treatment.print2Pl(images_02[0], equalized_image)
+# cv2.imwrite('output/quest_2.2.jpg',equalized_image)
+
+
+ 
--- src/imageTreatment.py
+++ src/imageTreatment.py
@@ -1,4 +1,4 @@
-import cv2
+import cv2 
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -6,122 +6,68 @@ class imageTreatment:
     def __init__(self) -> None:
         pass
 
-    def fill(self, image):
-        try:
-            matriz_imagem = np.array(image)
-            matriz_final = np.zeros((matriz_imagem.shape[0]*2,matriz_imagem.shape[1]*2,matriz_imagem.shape[2]))
-            matriz_final[::2, ::2, :] = matriz_imagem
-                        
-            return matriz_final
-        except Exception as erro:
-            print("Ocorreu um erro:", erro)
+    def equalize(self, image):
+        # Calcular o histograma
+        histogram, bins = np.histogram(image.flatten(), bins=256, range=[0, 256])
+        # Calcular a CDF
+        cdf = histogram.cumsum()
+        # Normalizar a CDF
+        cdf_normalized = cdf / cdf.max()
+        # Aplicar a equalização de histograma
+        equalized_image = np.interp(image.flatten(), bins[:-1], cdf_normalized * 255).reshape(image.shape).astype(np.uint8)
+        return equalized_image, histogram, cdf_normalized
 
-    def verifica_row(self, matriz):
-        result = []
-        for row in range(len(matriz)):
-            if(all(elemento == 0 for elemento in matriz[row])):
-                result.append(row)
+    def gamma(self, image,gamma):
+        gamma_table = np.array([((i/255.0) ** (1/gamma)) * 255 for i in np.arange(0, 256)]).astype("uint8")
+        corrected_image = cv2.LUT(image, gamma_table)
+        return corrected_image
 
-        return result
+    def printIm(self, tag, image):
+        # Plotar imagem equalizada com imshow ( equalização )
+        cv2.imshow(tag, image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
-    def verifica_coll(self, matriz):
-        result = []
-        for row in range(len(matriz)):
-            if( np.all(matriz[:, row] == 0) ):
-                result.append(row)
+    def print2Im(self, old_image, new_image, arg_a ='Imagem Original', arg_b ='Imagem Corrigida'):
+        # Mostra a imagem original e a imagem corrigida ( gama )
+        cv2.imshow(arg_a, old_image)
+        cv2.imshow(arg_b, new_image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
-        return result
+    def printPl(self, image, arg_a ='Intensidade', arg_b ='Frequência'):
+        # Plotar  imagem com plt ( histograma || CDF )
+        plt.plot(image)
+        plt.xlabel(arg_a)
+        plt.ylabel(arg_b)
+        plt.show()
 
-    def odd_fill(self, matriz, white_lines):
-        for row in white_lines:
-            linha = np.copy(matriz[row-1])
-            matriz[row] = linha
-                        
-        return matriz
+    def print2Pl(self, old_image, new_image, arg_a ='Imagem original', arg_b ='Imagem equalizada'):
+        # Exibir a imagem original e a imagem equalizada ( equalização )
+        plt.subplot(1, 2, 1)
+        plt.imshow(old_image, cmap='gray')
+        plt.title(arg_a)
+        plt.subplot(1, 2, 2)
+        plt.imshow(new_image, cmap='gray')
+        plt.title(arg_b)
+        plt.show()
 
-    def pair_fill(self, matriz, white_coll):
-        for row in range(len(matriz)):
-            for item in white_coll:
-                matriz[row][item] = matriz[row][item-1]
-    
-        return matriz
+# # __________main__________
+# image = cv2.imread(r'../input/car.png',0)
+# equalized_image, histogram, cdf = equalize(image)
+# corrected_image = gamma(image, 0.4)
 
-    def m_odd_fill(self, matriz, white_lines):
-        for row in white_lines:
-            linha = np.copy(matriz[row-1])
-            linha_2 = np.copy(matriz[row-1])
-            if(linha_2 is not None):
-                linha = (linha + linha_2)/2
-            matriz[row] = linha
-                        
-        return matriz
+# # printa imagem equalizada
+# # printIm('imagem equalizada', equalized_image) 
+# # print2Pl(image, equalized_image)
 
-    def m_pair_fill(self, matriz, white_coll):
-        for row in range(len(matriz)):
-            for item in white_coll:
-                cell = matriz[row][item-1]
+# # # print histograma e cdf
+# # printPl(cdf)
 
-                if(item+1 < len(matriz)):
-                    cell_2 = matriz[row][item+1]
-                    cell = (cell + cell_2)/2
-                
-                matriz[row][item] = cell
-    
-        return matriz
+# # print gama
+# print2Im(image, corrected_image)
 
-    def tam2(self, image):
-        fill_image = self.fill(image)
 
-        for x in range(fill_image.shape[2]):
-            white_lines = self.verifica_row(fill_image[:,:,x])
-            fill_image[:,:,x] = self.odd_fill(fill_image[:,:,x], white_lines)
-            white_coll = self.verifica_coll(fill_image[:,:,x])
-            fill_image[:,:,x] = self.pair_fill(fill_image[:,:,x], white_coll)
 
-        return fill_image
 
-    def tamm(self, image):
-        fill_image = self.fill(image)
 
-        for x in range(fill_image.shape[2]):
-            white_lines = self.verifica_row(fill_image[:,:,x])
-            fill_image[:,:,x] = self.m_odd_fill(fill_image[:,:,x], white_lines)
-            white_coll = self.verifica_coll(fill_image[:,:,x])
-            fill_image[:,:,x] = self.m_pair_fill(fill_image[:,:,x], white_coll)
-
-        return fill_image
-
-    def fill_matriz(self, matriz, image_b):
-        for z in range(image_b.shape[2]):
-            for x in range(image_b.shape[1]):
-                for y in range(image_b.shape[0]):
-                    matriz[((x+1)*2)-1, ((y+1)*2)-1, z] = image_b[x,y,z]
-        
-        return matriz
-
-    def med_matriz(self, matriz):
-        for z in range(matriz.shape[2]):
-            for x in range(matriz.shape[1]):
-                for y in range(matriz.shape[0]):
-                    if( matriz[x,y,z] == 0 ):
-                        result = 0
-                        idx = 0
-
-                        for cell in range(-1,1,2):
-                            if( x+cell >= 0 and x+cell < matriz.shape[1]):
-                                result += matriz[x+cell,y,z]
-                                idx+=1
-                            if( y+cell >= 0 and  y+cell < matriz.shape[0]):
-                                result += matriz[x,y+cell,z]
-                                idx+=1
-
-                        matriz[x,y,z] = result/idx
-
-        return matriz
-        
-    def merge_image(self, image_a, image_b):
-        fill_image = self.fill(image_a)
-        self.fill_matriz(fill_image, image_b)
-        self.med_matriz(fill_image)
-
-        return fill_image
